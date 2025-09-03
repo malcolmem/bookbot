0ed3c892628ff14b716fda784121a832946e6b9c
malcolmem <malcolmemiller@gmail.com>
2025-09-03 01:42:33 -0500

    completed bookbot

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from stats import get_num_words, get_chars_dict, get_sorted_dict
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
